7fac9bb17cf81bd819745c7d877033b5745c880d
SureshBoddu-DataEng <sureshsurya595@gmail.com>
2021-03-09 11:55:12 +0530

    fix

--- rdd/scholaship_recipient_filter_join.py
+++ rdd/scholaship_recipient_filter_join.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
 
     coursesRDD = spark.sparkContext.textFile("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/course.csv")
-    coursesRDD.show();
+    coursesRDD.collect();
 
     coursesRDD \
         .toDF(["id", "name"]) \
